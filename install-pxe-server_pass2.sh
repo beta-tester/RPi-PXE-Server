7a7f6bde33e5402cbdeeb1df0883535f7ba0cc3e
beta-tester <alpha-beta-release@gmx.net>
2017-12-24 07:42:24 +0100

    add: DEBIAN_KVER

--- install-pxe-server_pass2.sh
+++ install-pxe-server_pass2.sh
@@ -21,7 +21,7 @@
 #               http://tinycorelinux.net/9.x/armv7/releases/RPi/
 # clonezilla    http://clonezilla.org/
 #
-# v2017-12-11
+# v2017-12-24
 #
 # known issues:
 #
@@ -128,7 +128,7 @@ UBUNTU_NONPAE_URL=
 
 DEBIAN_X64=debian-x64
 DEBIAN_X64_URL=http://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.3.0-amd64-lxde.iso
-
+DEBIAN_KVER=4.9.0-4
 DEBIAN_X86=debian-x86
 DEBIAN_X86_URL=http://cdimage.debian.org/debian-cd/current-live/i386/iso-hybrid/debian-live-9.3.0-i386-lxde.iso
 
@@ -540,13 +540,13 @@ EOF";
     fi
 
     if [ -f "$FILE_MENU" ] \
-    && [ -f "$DST_NFS_ETH0/$DEBIAN_X64/live/vmlinuz-4.9.0-4-amd64" ]; then
+    && [ -f "$DST_NFS_ETH0/$DEBIAN_X64/live/vmlinuz-$DEBIAN_KVER-amd64" ]; then
         echo  -e "\e[36m    add $DEBIAN_X64\e[0m";
         sudo sh -c "cat << EOF  >> $FILE_MENU
 ########################################
 LABEL Debian x64
-    KERNEL $NFS_ETH0/$DEBIAN_X64/live/vmlinuz-4.9.0-4-amd64
-    APPEND initrd=$NFS_ETH0/$DEBIAN_X64/live/initrd.img-4.9.0-4-amd64 netboot=nfs nfsroot=$IP_ETH0:$DST_NFS_ETH0/$DEBIAN_X64 ro boot=live config -- locales=de_DE.UTF-8 keyboard-layouts=de utc=no timezone=Europe/Berlin
+    KERNEL $NFS_ETH0/$DEBIAN_X64/live/vmlinuz-$DEBIAN_KVER-amd64
+    APPEND initrd=$NFS_ETH0/$DEBIAN_X64/live/initrd.img-$DEBIAN_KVER-amd64 netboot=nfs nfsroot=$IP_ETH0:$DST_NFS_ETH0/$DEBIAN_X64 ro boot=live config -- locales=de_DE.UTF-8 keyboard-layouts=de utc=no timezone=Europe/Berlin
     TEXT HELP
         Boot to Debian x64 Live LXDE
         User: user, Password: live
@@ -555,13 +555,13 @@ EOF";
     fi
 
     if [ -f "$FILE_MENU" ] \
-    && [ -f "$DST_NFS_ETH0/$DEBIAN_X86/live/vmlinuz-4.9.0-4-686" ]; then
+    && [ -f "$DST_NFS_ETH0/$DEBIAN_X86/live/vmlinuz-$DEBIAN_KVER-686" ]; then
         echo  -e "\e[36m    add $DEBIAN_X86\e[0m";
         sudo sh -c "cat << EOF  >> $FILE_MENU
 ########################################
 LABEL Debian x86
-    KERNEL $NFS_ETH0/$DEBIAN_X86/live/vmlinuz-4.9.0-4-686
-    APPEND initrd=$NFS_ETH0/$DEBIAN_X86/live/initrd.img-4.9.0-4-686 netboot=nfs nfsroot=$IP_ETH0:$DST_NFS_ETH0/$DEBIAN_X86 ro boot=live config -- locales=de_DE.UTF-8 keyboard-layouts=de utc=no timezone=Europe/Berlin
+    KERNEL $NFS_ETH0/$DEBIAN_X86/live/vmlinuz-$DEBIAN_KVER-686
+    APPEND initrd=$NFS_ETH0/$DEBIAN_X86/live/initrd.img-$DEBIAN_KVER-686 netboot=nfs nfsroot=$IP_ETH0:$DST_NFS_ETH0/$DEBIAN_X86 ro boot=live config -- locales=de_DE.UTF-8 keyboard-layouts=de utc=no timezone=Europe/Berlin
     TEXT HELP
         Boot to Debian x86 Live LXDE
         User: user, Password: live
