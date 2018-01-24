0aacf7d1e65cd7cb5f420f2ac1eb6ff9e2b3fcf0
beta-tester <alpha-beta-release@gmx.net>
2018-01-24 21:39:15 +0100

    update url

--- install-pxe-server_pass2.sh
+++ install-pxe-server_pass2.sh
@@ -24,7 +24,7 @@
 #               http://tinycorelinux.net/9.x/armv7/releases/RPi/
 # clonezilla    http://clonezilla.org/
 #
-# v2018-01-18
+# v2018-01-24
 #
 # known issues:
 #    overlayfs can not get exported via nfs
@@ -201,7 +201,7 @@ FEDORA_X64=fedora-x64
 FEDORA_X64_URL=https://download.fedoraproject.org/pub/fedora/linux/releases/27/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-27-1.6.iso
 
 TAILS_X64=tails-x64
-TAILS_X64_URL=https://mirrors.kernel.org/tails/stable/tails-amd64-3.4/tails-amd64-3.4.iso
+TAILS_X64_URL=https://mirrors.kernel.org/tails/stable/tails-amd64-3.5/tails-amd64-3.5.iso
 
 CENTOS_X64=centos-x64
 CENTOS_X64_URL=http://ftp.rrzn.uni-hannover.de/centos/7/isos/x86_64/CentOS-7-x86_64-LiveGNOME-1708.iso
