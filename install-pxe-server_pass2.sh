c814c54ececc1d55ef6af699e42713f0997a0020
beta-tester <alpha-beta-release@gmx.net>
2018-10-18 21:57:04 +0200

    fix initrd of lubuntu LTS 18.04 x86

--- install-pxe-server_pass2.sh
+++ install-pxe-server_pass2.sh
@@ -837,7 +837,7 @@ EOF";
 LABEL $LUBUNTU_LTS_X86
     MENU LABEL lubuntu LTS x86
     KERNEL $FILE_BASE$NFS_ETH0/$LUBUNTU_LTS_X86/casper/vmlinuz
-    INITRD $FILE_BASE$NFS_ETH0/$LUBUNTU_LTS_X86/casper/initrd.lz
+    INITRD $FILE_BASE$NFS_ETH0/$LUBUNTU_LTS_X86/casper/initrd
     APPEND nfsroot=$IP_ETH0:$DST_NFS_ETH0/$LUBUNTU_LTS_X86 ro netboot=nfs file=/cdrom/preseed/lubuntu.seed boot=casper systemd.mask=tmp.mount -- debian-installer/language=$CUSTOM_LANG console-setup/layoutcode=$CUSTOM_LANG keyboard-configuration/layoutcode=$CUSTOM_LANG keyboard-configuration/variant=$CUSTOM_LANG_WRITTEN
     TEXT HELP
         Boot to lubuntu LTS x86 Live
