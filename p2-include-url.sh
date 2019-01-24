7aa29e131711938a4f924715ded817d88a4ebb71
beta-tester <alpha-beta-release@gmx.net>
2019-01-25 00:22:56 +0100

    updated url (debian)

--- p2-include-url.sh
+++ p2-include-url.sh
@@ -9,7 +9,7 @@ then
 fi
 ##########################################################################
 
-#v 2019-01-22
+# v 2019-01-25
 
 ##########################################################################
 ##########################################################################
@@ -30,10 +30,10 @@ CLONEZILLA_X86_URL=https://downloads.sourceforge.net/project/clonezilla/clonezil
 DEBIAN_KVER=4.9.0-8
 
 DEBIAN_X64=debian-x64
-DEBIAN_X64_URL=https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.6.0-amd64-lxde.iso
+DEBIAN_X64_URL=https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-9.7.0-amd64-lxde.iso
 
 DEBIAN_X86=debian-x86
-DEBIAN_X86_URL=https://cdimage.debian.org/debian-cd/current-live/i386/iso-hybrid/debian-live-9.6.0-i386-lxde.iso
+DEBIAN_X86_URL=https://cdimage.debian.org/debian-cd/current-live/i386/iso-hybrid/debian-live-9.7.0-i386-lxde.iso
 
 DEFTZ_X64=deftz-x64
 DEFTZ_X64_URL=https://na.mirror.garr.it/mirrors/deft/zero/deftZ-2018-2.iso
