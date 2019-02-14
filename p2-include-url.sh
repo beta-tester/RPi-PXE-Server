7a2bda944fa70e356860cdeead7af09abfeb4fa4
beta-tester <alpha-beta-release@gmx.net>
2019-02-14 17:14:20 +0100

    updated url (tails)

--- p2-include-url.sh
+++ p2-include-url.sh
@@ -9,7 +9,7 @@ then
 fi
 ##########################################################################
 
-# v 2019-02-11
+# v 2019-02-14
 
 ##########################################################################
 ##########################################################################
@@ -151,7 +151,7 @@ OPENSUSE_X64=opensuse-x64
 OPENSUSE_X64_URL=https://download.opensuse.org/distribution/openSUSE-current/live/openSUSE-Leap-15.0-GNOME-Live-x86_64-Current.iso
 
 TAILS_X64=tails-x64
-TAILS_X64_URL=https://mirrors.edge.kernel.org/tails/stable/tails-amd64-3.12/tails-amd64-3.12.iso
+TAILS_X64_URL=https://mirrors.edge.kernel.org/tails/stable/tails-amd64-3.12.1/tails-amd64-3.12.1.iso
 
 
 ##########################################################################
