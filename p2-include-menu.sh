e25a0b54295ada13008141a99cd99a41ddcc2c3c
beta-tester <alpha-beta-release@gmx.net>
2019-10-23 22:32:46 +0200

    issue #27 (2)

--- p2-include-menu.sh
+++ p2-include-menu.sh
@@ -281,7 +281,7 @@ fi
 if [ -f "$FILE_MENU" ] \
 && [ -f "$DST_NFS_ETH0/$KNOPPIX_X86/boot/isolinux/linux" ]; then
     echo  -e "\e[36m    add $KNOPPIX_X86\e[0m";
-    #[ -f "$DST_NFS_ETH0/knoppix-x86-miniroot.gz" ] || ( sudo wget -O $DST_NFS_ETH0/knoppix-x86-miniroot.gz https://github.com/beta-tester/RPi-PXE-Server/files/3754352/knoppix-x86-miniroot.gz );
+    ! [ -f "$DST_NFS_ETH0/knoppix-x86-miniroot.gz" ] || ( sudo wget -O $DST_NFS_ETH0/knoppix-x86-miniroot.gz https://github.com/beta-tester/RPi-PXE-Server/files/3764311/knoppix-x86-miniroot.gz );
     sudo sh -c "cat << EOF  >> $FILE_MENU
     ########################################
     ## INFO: https://github.com/beta-tester/RPi-PXE-Server/issues/27
@@ -291,7 +291,7 @@ if [ -f "$FILE_MENU" ] \
         MENU LABEL Knoppix x86
         KERNEL $FILE_BASE$NFS_ETH0/$KNOPPIX_X86/boot/isolinux/linux
         INITRD $FILE_BASE$NFS_ETH0/knoppix-x86-miniroot.gz
-        APPEND secure nfsdir=$IP_ETH0:$DST_NFS_ETH0/$KNOPPIX_X86 nodhcp lang=de ramdisk_size=100000 init=/sbin/init apm=power-off nomce loglevel=1 libata.force=noncq tz=localtime hpsa.hpsa_allow_any=1 BOOT_IMAGE=knoppix
+        APPEND secure nfsdir=$IP_ETH0:$DST_NFS_ETH0/$KNOPPIX_X86 nodhcp lang=de mem=800M ramdisk_size=100000 init=/sbin/init apm=power-off nomce loglevel=1 libata.force=noncq tz=localtime hpsa.hpsa_allow_any=1 BOOT_IMAGE=knoppix
         TEXT HELP
             Boot to Knoppix x86 Live
         ENDTEXT
