eec66f7320e9026d4a21be2afd641e75c50fc0ba
beta-tester <alpha-beta-release@gmx.net>
2020-05-27 20:05:30 +0200

    updated url (sysrescue)

--- p2-include-url.sh
+++ p2-include-url.sh
@@ -9,7 +9,7 @@ then
 fi
 ##########################################################################
 
-# v 2020-05-17
+# v 2020-05-27
 
 ##########################################################################
 # winpe                https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install
@@ -194,7 +194,7 @@ RPDESKTOP_X86_SUM_TYPE=sha256
 
 
 SYSTEMRESCUE_X64=systemrescue-x64
-SYSTEMRESCUE_X64_URL=https://downloads.sourceforge.net/project/systemrescuecd/sysresccd-x86/6.1.3/systemrescuecd-amd64-6.1.3.iso
+SYSTEMRESCUE_X64_URL=https://downloads.sourceforge.net/project/systemrescuecd/sysresccd-x86/6.1.4/systemrescuecd-amd64-6.1.4.iso
 
 
 TAILS_X64=tails-x64
