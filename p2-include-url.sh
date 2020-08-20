7a9824e26df2cb0325c5365f0d3cabb6ffba9d8b
beta-tester <alpha-beta-release@gmx.net>
2020-08-20 13:17:32 +0200

    updated url (ubuntu)

--- p2-include-url.sh
+++ p2-include-url.sh
@@ -142,29 +142,24 @@ KNOPPIX_X86_SUM_TYPE=sha256
 
 
 LUBUNTU_DAILY_X64=lubuntu-daily-x64
-LUBUNTU_DAILY_X64_URL=https://cdimage.ubuntu.com/lubuntu/daily-live/pending/focal-desktop-amd64.iso
+LUBUNTU_DAILY_X64_URL=https://cdimage.ubuntu.com/lubuntu/daily-live/pending/groovy-desktop-amd64.iso
 LUBUNTU_DAILY_X64_SUM=https://cdimage.ubuntu.com/lubuntu/daily-live/pending/SHA256SUMS
 LUBUNTU_DAILY_X64_SUM_TYPE=sha256
 
 LUBUNTU_LTS_X64=lubuntu-lts-x64
-LUBUNTU_LTS_X64_URL=https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04-desktop-amd64.iso
+LUBUNTU_LTS_X64_URL=https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.1-desktop-amd64.iso
 LUBUNTU_LTS_X64_SUM=https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/SHA256SUMS
 LUBUNTU_LTS_X64_SUM_TYPE=sha256
 
 LUBUNTU_X64=lubuntu-x64
-LUBUNTU_X64_URL=https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04-desktop-amd64.iso
+LUBUNTU_X64_URL=https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.1-desktop-amd64.iso
 LUBUNTU_X64_SUM=https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/SHA256SUMS
 LUBUNTU_X64_SUM_TYPE=sha256
 
 LUBUNTU_LTS_X86=lubuntu-lts-x86
-LUBUNTU_LTS_X86_URL=https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/lubuntu-18.04.3-desktop-i386.iso
+LUBUNTU_LTS_X86_URL=https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/lubuntu-18.04.5-desktop-i386.iso
 LUBUNTU_LTS_X86_SUM=https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/SHA256SUMS
 LUBUNTU_LTS_X86_SUM_TYPE=sha256
-
-LUBUNTU_X86=lubuntu-x86
-LUBUNTU_X86_URL=https://cdimage.ubuntu.com/lubuntu/releases/18.10/release/lubuntu-18.10-desktop-i386.iso
-LUBUNTU_X86_SUM=https://cdimage.ubuntu.com/lubuntu/releases/18.10/release/SHA256SUMS
-LUBUNTU_X86_SUM_TYPE=sha256
 
 
 MINT_X64=mint-x64
@@ -240,22 +235,22 @@ UBUNTU_FWTS_SUM=http://fwts.ubuntu.com/fwts-live/SHA256SUM
 UBUNTU_FWTS_SUM_TYPE=sha256
 
 UBUNTU_LTS_X64=ubuntu-lts-x64
-UBUNTU_LTS_X64_URL=https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso
+UBUNTU_LTS_X64_URL=https://releases.ubuntu.com/20.04/ubuntu-20.04.1-desktop-amd64.iso
 UBUNTU_LTS_X64_SUM=https://releases.ubuntu.com/20.04/SHA256SUMS
 UBUNTU_LTS_X64_SUM_TYPE=sha256
 
 UBUNTU_STUDIO_DAILY_X64=ubuntu-studio-daily-x64
-UBUNTU_STUDIO_DAILY_X64_URL=https://cdimage.ubuntu.com/ubuntustudio/dvd/pending/focal-dvd-amd64.iso
+UBUNTU_STUDIO_DAILY_X64_URL=https://cdimage.ubuntu.com/ubuntustudio/dvd/pending/groovy-dvd-amd64.iso
 UBUNTU_STUDIO_DAILY_X64_SUM=https://cdimage.ubuntu.com/ubuntustudio/dvd/pending/SHA256SUMS
 UBUNTU_STUDIO_DAILY_X64_SUM_TYPE=sha256
 
 UBUNTU_STUDIO_X64=ubuntu-studio-x64
-UBUNTU_STUDIO_X64_URL=https://cdimage.ubuntu.com/ubuntustudio/releases/20.04/release/ubuntustudio-20.04-dvd-amd64.iso
+UBUNTU_STUDIO_X64_URL=https://cdimage.ubuntu.com/ubuntustudio/releases/20.04/release/ubuntustudio-20.04.1-dvd-amd64.iso
 UBUNTU_STUDIO_X64_SUM=https://cdimage.ubuntu.com/ubuntustudio/releases/20.04/release/SHA256SUMS
 UBUNTU_STUDIO_X64_SUM_TYPE=sha256
 
 UBUNTU_X64=ubuntu-x64
-UBUNTU_X64_URL=https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso
+UBUNTU_X64_URL=https://releases.ubuntu.com/20.04/ubuntu-20.04.1-desktop-amd64.iso
 UBUNTU_X64_SUM=https://releases.ubuntu.com/20.04/SHA256SUMS
 UBUNTU_X64_SUM_TYPE=sha256
 
